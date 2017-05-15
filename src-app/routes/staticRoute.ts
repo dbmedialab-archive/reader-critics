1e0643ca211f342e915855cc95d4c139f12b3550
Philipp <philipp@sol.no>
2017-05-15 15:35:02 +0200

    Some styles, a nice header picture.

--- src-app/routes/staticRoute.ts
+++ src-app/routes/staticRoute.ts
@@ -5,13 +5,14 @@ import * as app from 'util/applib';
 
 const router = express.Router();
 
+router.use('/images', express.static(path.join(app.rootPath, 'tmp/images')));
+router.use('/styles', express.static(path.join(app.rootPath, 'tmp/styles')));
+
 // We don't bundle frontend libraries together with the compiled sources, but rather host
 // them from static endpoints. Fair tradeoff between enabled browser caching but not using
 // a CDN for those libs and being able to upgrade them easily through NPM or Yarn locally.
 router.use('/react', express.static(path.join(app.rootPath, 'node_modules/react/dist/')));
 router.use('/react', express.static(path.join(app.rootPath, 'node_modules/react-dom/dist/')));
-
-router.use('/styles', express.static(path.join(app.rootPath, 'tmp/styles')));
 
 router.use('/', express.static(path.join(app.rootPath, 'out/front')));
 
