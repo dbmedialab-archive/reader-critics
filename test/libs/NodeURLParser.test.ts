649c22b6e0bf8cb2ae50b2ea14e6054d9e10b48b
Philipp <philipp@sol.no>
2017-05-26 08:42:30 +0200

    This has still (almost) nothing to do with the task, but it fixes the usage of new URL(" compared to url.parse(). Also added a few additional tests to check for exceptions and removal of default port numbers.

--- test/libs/NodeURLParser.test.ts
+++ test/libs/NodeURLParser.test.ts
@@ -13,16 +13,21 @@ interface TestRecord {
 
 // Declare the test data
 
-const examples : TestRecord[] = [
+const portProbes : string[] = [
+	'http://example.com:80/narf',
+	'https://secure.net:443/argh',
+];
+
+const parserProbes : TestRecord[] = [
 	{
 		url: 'http://www.dagbladet.no/nyheter/le-pen-blir-mer-kaos/67532944',
 		host: 'www.dagbladet.no',
-		port: n => n === null,
+		port: n => n === '',
 	},
 	{
 		url: 'https://whazzup.org/here/be/some',
 		host: 'whazzup.org',
-		port: n => n === null,
+		port: n => n === '',
 	},
 	{
 		url: 'http://some.hostname.de:1234/with/port',
@@ -32,7 +37,7 @@ const examples : TestRecord[] = [
 	{
 		url: 'http://default.example.com:80/with/default/port',
 		host: 'default.example.com',
-		port: n => parseInt(n) === 80,
+		port: n => n === '',
 	},
 	{
 		url: 'https://secure.hostname.de:4433/with/port',
@@ -42,26 +47,43 @@ const examples : TestRecord[] = [
 	{
 		url: 'https://secure.example.com:443/with/default/port',
 		host: 'secure.example.com',
-		port: n => parseInt(n) === 443,
+		port: n => n === '',
 	},
 	{
-		url: 'http://ungueltig.com:schmarrn/',
-		host: 'ungueltig.com',
-		port: n => n === null,
+		url: 'http://not-so-secure.example.com:443/with/https/port',
+		host: 'not-so-secure.example.com',
+		port: n => parseInt(n) === 443,
 	},
 ];
 
-// Test
+// Tests
 
 describe('Node «url» package', function() {
-	it('should parse an URL correctly', function() {
-		examples.forEach(testParser);
+
+	it('Throw a TypeError on a malformed URL', function() {
+		assert.throws(() => {
+			new URL('http://ungueltig.com:schmarrn/');
+		}, TypeError);
 	});
+
+	it('Throw away default ports', function() {
+		const testFn = (fromURL : string) => {
+			const parsed = new URL(fromURL);
+			assert.isString(parsed.port);
+			assert.lengthOf(parsed.port, 0);
+		};
+
+		portProbes.forEach(testFn);
+	});
+
+	it('Parse an URL correctly', function() {
+		const testFn = (record : TestRecord) => {
+			const parsed = new URL(record.url);
+			assert.strictEqual(parsed.hostname, record.host);
+			assert.isTrue(record.port(parsed.port), `for ${record.url}`);
+		};
+
+		parserProbes.forEach(testFn);
+	});
+
 });
-
-function testParser(record : TestRecord) {
-	const parsed = new URL(record.url);
-
-	assert.strictEqual(parsed.hostname, record.host);
-	assert.isTrue(record.port(parsed.port));
-}
