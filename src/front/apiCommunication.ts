551e26f6561a7a151dddc3fd2fdebdcd19d02011
Philipp <philipp@sol.no>
2017-06-13 08:49:52 +0200

    Extracted communication code into own module; begin collection all comm functions here.

---
+++ src/front/apiCommunication.ts
@@ -0,0 +1,24 @@
+import {
+	default as axios,
+	AxiosPromise,
+	AxiosResponse,
+} from 'axios';
+
+const rxUnencoded = /:\/\//;
+
+export function fetchArticle(url : string) : Promise<any> {
+	const encodedURL = rxUnencoded.test(url)
+		? encodeURIComponent(url)
+		: url;
+
+	return axios.get(`/api/article/?url=${encodedURL}`)
+	.then((resp : AxiosResponse) => {
+		const d = resp.data;
+
+		if (d.success === true) {
+			return d.data.article;
+		}
+
+		return Promise.reject(new Error(`Failed to load article at ${url}`));
+	});
+}
