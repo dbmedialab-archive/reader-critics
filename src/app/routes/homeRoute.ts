4f666dffa8ac7ef80de1b8b517e984c5fcf239e3
DonikaV <dov@ciklum.com>
2017-06-15 10:42:23 +0300

    Started working on Home Template

--- src/app/routes/homeRoute.ts
+++ src/app/routes/homeRoute.ts
@@ -5,6 +5,9 @@ import {
 } from 'express';
 
 import * as app from 'app/util/applib';
+import * as doT from 'dot';
+import * as path from 'path';
+import { readFileSync } from 'fs';
 
 const log = app.createLog();
 
@@ -24,13 +27,31 @@ homeRoute.get('/*', notFoundHandler);
 
 export default homeRoute;
 
+const mainTemplate = createMainTemplate();
+
+const styles = [
+];
+
+const scripts = [
+	'/static/react/react.js',
+	'/static/react/react-dom.js',
+	'/static/front.bundle.js',
+];
+
 // Main handler, checks for URL parameter and "empty" requests
 
 function homeHandler(requ : Request, resp : Response) {
 	log('Homepage router', requ.params);
-	resp.json({
-		status: 'show the project homepage here',
-	}).status(200).end();
+	resp.set('Content-Type', 'text/html');
+	resp.send(mainTemplate({
+		feedbackParam: JSON.stringify({
+			signed: 'NUdzNVJRdUdmTzd0ejFBWGwxS2tZRDVrRzBldTVnc0RDc2VheGdwego=',
+		}),
+		styles,
+		scripts,
+	}));
+
+	resp.status(200).end();
 }
 
 // Everything else
@@ -38,4 +59,13 @@ function homeHandler(requ : Request, resp : Response) {
 function notFoundHandler(requ : Request, resp : Response) {
 	log('404 not found');
 	resp.status(404).end('not found');
+}
+
+function createMainTemplate() {
+	// Currently loads the template from a static file.
+	// The template will later be determined dynamically based on website url / domain.
+	const templatePath = path.join(app.rootPath, 'assets/templates/home.html');
+	const templateRaw = readFileSync(templatePath);
+
+	return doT.template(templateRaw);
 }
