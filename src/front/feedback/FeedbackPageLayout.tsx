824ad1b1977a0d82665d4b48ceee0d7f185c5b02
Philipp <philipp@sol.no>
2017-06-19 18:52:19 +0200

    Moved FloatingActionButton to FinishButton

--- src/front/feedback/FeedbackPageLayout.tsx
+++ src/front/feedback/FeedbackPageLayout.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 
 import ArticleContent from '../section/ArticleContent';
-import FloatingActionButton from '../component/FloatingActionButton';
+import FinishButton from './FinishButton';
 import Footer from '../section/Footer';
 import Header from '../section/Header';
 
 const FeedbackPageLayout : React.StatelessComponent <any> = () => <div>
 	<Header />
 	<ArticleContent />
-	<FloatingActionButton/>
+	<FinishButton />
 	<Footer />
 </div>;
 
