adfe943b327b7f0dbc3edafdc65c953674bf26fa
Philipp <philipp@sol.no>
2017-06-19 18:56:36 +0200

    Moved Header to FeedbackHeader and made it a stateless component.

--- src/front/feedback/FeedbackPageLayout.tsx
+++ src/front/feedback/FeedbackPageLayout.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 
 import ArticleContent from '../section/ArticleContent';
+import FeedbackHeader from './FeedbackHeader';
+
 import FinishButton from './FinishButton';
 import Footer from '../section/Footer';
-import Header from '../section/Header';
 
 const FeedbackPageLayout : React.StatelessComponent <any> = () => <div>
-	<Header />
+	<FeedbackHeader />
 	<ArticleContent />
 	<FinishButton />
 	<Footer />
