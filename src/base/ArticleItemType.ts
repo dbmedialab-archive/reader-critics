5148600d248288adecbd30f5f35445bd9f6597e1
Philipp <philipp@sol.no>
2017-06-21 19:22:59 +0200

    Use TypeScript's new string enum feature (v2.4)

---
+++ src/base/ArticleItemType.ts
@@ -0,0 +1,16 @@
+enum ArticleItemType {
+
+	MainTitle = 'title',
+	SubTitle = 'subtitle',
+
+	LeadIn = 'lead',
+
+	SubHeading = 'subhead',
+	Paragraph = 'paragraph',
+	Figure = 'figure',
+
+}
+
+export default ArticleItemType;
+
+console.log(ArticleItemType);
