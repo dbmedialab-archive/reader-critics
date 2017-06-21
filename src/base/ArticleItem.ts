30991bead769de0a2b307e59306f6026525db76a
Philipp <philipp@sol.no>
2017-06-21 20:48:16 +0200

    New article-related types

---
+++ src/base/ArticleItem.ts
@@ -0,0 +1,13 @@
+import ArticleItemType from 'base/ArticleItemType';
+
+interface ArticleItem {
+	type : ArticleItemType;
+
+	order: {
+		item: number,
+		type: number,
+	},
+
+}
+
+export default ArticleItem;
