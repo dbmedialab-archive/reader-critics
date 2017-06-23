a716ca6788a2ccc9c1ac8c94500231ed12d125ee
DonikaV <dov@ciklum.com>
2017-06-23 15:45:08 +0300

    fetch whatwg

--- src/front/apiCommunication.ts
+++ src/front/apiCommunication.ts
@@ -1,31 +1,21 @@
-import {
-	default as axios,
-	AxiosResponse,
-} from 'axios';
-
 import 'whatwg-fetch'
-
-console.log(fetch);
-
 
 
 const rxUnencoded = /:\/\//;
 
-export function fetchArticle(url : string, version : string) : Promise<any> {
+export function fetchArticle(url: string, version: string): Promise<any> {
 	const encodedURL = rxUnencoded.test(url)
 		? encodeURIComponent(url)
 		: url;
 
-	return axios.get(`/api/article/?url=${encodedURL}`)
-	.then((resp : AxiosResponse) => {
-		const d = resp.data;
-
-		if (d.success === true) {
-			return d.data.article;
-		}
-
-		return Promise.reject(new Error(`Failed to load article at ${url}`));
-	});
+	return fetch(`/api/article/?url=${encodedURL}`)
+		.then(function (response: Response) {
+			return response.json()
+		}).then(function (json) {
+			console.log('parsed json', json)
+		}).catch(function (ex) {
+			console.log('parsing failed', ex)
+		})
 }
 
 /*export function sendSuggestion(data: any) : Promise<any> {
@@ -47,26 +37,49 @@ export function fetchArticle(url : string, version : string) : Promise<any> {
 }*/
 
 
-export const sendSuggestion = ((data: any): Promise<any> => {
+/*export const sendSuggestion = ((data: any): Promise<any> => {
 	console.log(JSON.stringify(data));
 	const apiUrl = `/api/suggest/`;
 	return fetch(apiUrl, {
 		method: 'POST',
-		headers: {'Content-Type': 'application/json'},
+		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(data)
-	}).then(checkStatus)
-		.then(r => r.json())
-		.then(data => {
-			console.log(data);
-		});
+	}).then(function (response: Response) {
+		console.log(response);
+		return response.json()
+	}).then(function (json) {
+		console.log('parsed json', json)
+	}).catch(function (ex) {
+		console.log('parsing failed', ex)
+	})
+});*/
+
+export const sendSuggestion = ((data: any): Promise<any> => {
+	const apiUrl = `/api/suggest/`;
+	return fetch(apiUrl, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(data)
+	})
+		.then(checkStatus)
+		.then(parseJSON)
+		.then(function (data) {
+			console.log('request succeeded with JSON response', data)
+		}).catch(function (error) {
+			console.log('request failed', error)
+		})
 });
 
-const checkStatus = ((response: any) => {
-  if (response.ok) {
-	  return response;
+
+function checkStatus(response: Response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response
   } else {
-    var error = new Error(response.statusText);
-	console.log(error);
-    //return Promise.reject(error);
+    var error = new Error(response.statusText)
+	throw error
   }
-})
+}
+ 
+function parseJSON(response) {
+  return response.json()
+}
