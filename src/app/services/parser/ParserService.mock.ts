ab3eb48043a7ad1d9344b0ef5ee6a6a384ed70bb
Philipp <philipp@sol.no>
2017-06-28 15:47:55 +0200

    Use live implementation of getParserFor()

--- src/app/services/parser/ParserService.mock.ts
+++ src/app/services/parser/ParserService.mock.ts
@@ -1,6 +1,1 @@
-import Parser from 'base/Parser';
-import Website from 'base/Website';
-
-export function getParserFor (website : Website) : Promise <Parser> {
-	return Promise.resolve(null);  // Pending implementation :-)
-}
+export { default as getParserFor } from './live/getParserFor';
