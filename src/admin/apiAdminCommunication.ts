25f5c1b3dc5c9a9a8addecb739972f1019f89e91
Dimitriy Borshchov <dbor@ciklum.com>
2017-07-04 11:10:32 +0300

    RC-77 - Minor fixes

--- src/admin/apiAdminCommunication.ts
+++ src/admin/apiAdminCommunication.ts
@@ -1,6 +1,14 @@
 import 'whatwg-fetch';
+import AuthenticationError from 'admin/errors/AuthenticationError';
 
 const rxUnencoded = /:\/\//;
+
+function authCheck(response) {
+	if (response.status !== 401) {
+		return response;
+	}
+	window.location.href = '/admin/login';
+}
 
 function status(response) {
 	if (response.status >= 200 && response.status < 300) {
