7fea2424a73cfebf0bcfc13dad105cb12a9f72fa
Philipp <phgroe@gmx.net>
2017-07-06 17:36:44 +0200

    Removed export symbol, interfaces should only "export default"

--- src/base/Article.ts
+++ src/base/Article.ts
@@ -2,8 +2,7 @@ import ArticleAuthor from './ArticleAuthor';
 import ArticleItem from './ArticleItem';
 import ArticleURL from './ArticleURL';
 
-export interface Article {
-
+interface Article {
 	// Defining a unique version of one article
 	url : ArticleURL;
 	version : string;
@@ -13,7 +12,6 @@ export interface Article {
 
 	// Contents - Title, subtitle, everything is picked up as an item
 	items : ArticleItem[];
-
 }
 
 export default Article;
