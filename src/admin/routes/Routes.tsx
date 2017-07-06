c3ec93dca6cb89d90b51ceaeaae094da4f471374
tcat <trustut@gmail.com>
2017-07-06 21:40:30 +0300

    up

--- src/admin/routes/Routes.tsx
+++ src/admin/routes/Routes.tsx
@@ -3,9 +3,7 @@ import { Route, Switch } from 'react-router';
 
 import Users from 'admin/components/user/Users';
 
-const Routes : React.StatelessComponent <JSX.Element> =	() =>
+export const Routes : React.StatelessComponent <JSX.Element> =	() =>
 	<Switch>
 			<Route path="/users" component={Users}/>
 	</Switch>;
-
-export Routes;
