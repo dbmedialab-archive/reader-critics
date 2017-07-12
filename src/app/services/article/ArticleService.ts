505246d8c621f7b78d76a4de12b6f4849922ebfe
Philipp <phgroe@gmx.net>
2017-07-12 17:51:23 +0200

    Linter complained

--- src/app/services/article/ArticleService.ts
+++ src/app/services/article/ArticleService.ts
@@ -23,14 +23,11 @@ import {
 } from 'base';
 
 interface ArticleService {
-
 	download(url : ArticleURL) : Promise <string>;
 	fetch(website : Website, url : ArticleURL) : Promise <Article>;
 
 	load(url : ArticleURL, version : string) : Promise <Article>;
 	save(website : Website, article : Article) : Promise <void>;
-
 }
 
 export default ArticleService;
-
