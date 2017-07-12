4c9c439f2408a777d25ecad277946b7359579fb6
tcat <trustut@gmail.com>
2017-07-12 17:26:56 +0300

    cant use props from connect.

--- src/admin/components/layout/LayoutComponent.tsx
+++ src/admin/components/layout/LayoutComponent.tsx
@@ -43,43 +43,47 @@ import {ICON_TYPE} from 'react-preloader-icon';
 import SidebarComponent from 'admin/components/layout/SidebarComponent';
 import TopbarComponent from 'admin/components/layout/TopbarComponent';
 
-const LayoutComponent : React.StatelessComponent <any> = () =>
-		<div className="off-canvas-wrap">
-			<div className="inner-wrap">
-				<div onClick={this.bodyClicked} style={{height: '100%'}}>
+class LayoutComponent extends React.Component <any, any> {
+	render() : JSX.Element {
+		return(
+			<div className="off-canvas-wrap">
+				<div className="inner-wrap">
+					<div style={{height: '100%'}}>
 
-					{this.props.mainPreloader.isVisible?
-						<div className="preloaderSection">
-								<PreloaderIcon
-										type={ICON_TYPE.OVAL}
-										size={50}
-										strokeWidth={3}
-										strokeColor={this.props.mainPreloader.color}
-										duration={600}
-								/>
-						</div>
-					:null}
-					<Helmet title={this.props.pageTitle} />
-					<SidebarComponent>
-						{this.props.toSidebarContent}
-					</SidebarComponent>
-					<div className="paper-bg wrap-fluid" >
-						<TopbarComponent
-							isSubmenuOpen={this.props.isSubmenuOpen}
-							isAccountMenuOpen={this.props.isAccountMenuOpen}
-							currentUser={this.props.currentUser}
-						/>
-						<div className="content-wrapper">
-							<div className="content">
-								{this.props.mainPreloader.isVisible}
-								{this.props.children}
+						{this.props.mainPreloader.isVisible?
+							<div className="preloaderSection">
+									<PreloaderIcon
+											type={ICON_TYPE.OVAL}
+											size={50}
+											strokeWidth={3}
+											strokeColor={this.props.mainPreloader.color}
+											duration={600}
+									/>
+							</div>
+						:null}
+						<Helmet title={this.props.pageTitle} />
+						<SidebarComponent>
+							{this.props.toSidebarContent}
+						</SidebarComponent>
+						<div className="paper-bg wrap-fluid" >
+							<TopbarComponent
+								isSubmenuOpen={this.props.isSubmenuOpen}
+								isAccountMenuOpen={this.props.isAccountMenuOpen}
+								currentUser={this.props.currentUser}
+							/>
+							<div className="content-wrapper">
+								<div className="content">
+									{this.props.mainPreloader.isVisible}
+									{this.props.children}
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>;
-
+		);
+	}
+}
 const mapStateToProps = (state, ownProps) => {
 	return {
 		mainPreloader: {
@@ -94,10 +98,10 @@ const mapStateToProps = (state, ownProps) => {
 			role: 1,
 		},
 	};
-};
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => {
 	return {};
-};
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(LayoutComponent);
--- src/admin/components/layout/TopbarComponent.tsx
+++ src/admin/components/layout/TopbarComponent.tsx
@@ -20,48 +20,49 @@ import * as React from 'react';
 
 import MainMenuComponent from 'admin/components/layout/MainMenuComponent';
 
-const TopbarComponent : React.StatelessComponent <any> = () => {
-	const userName = this.props.currentUser.name;
-	let submenuClass = 'top-bar-left show-for-small-only';
-	submenuClass += (this.props.isSubmenuOpen)?' open':'';
-	let accountMenuClass:string = 'has-dropdown bg-white';
-	accountMenuClass += (this.props.isAccountMenuOpen)?' open':'';
-	return(
-		<div className="top-bar-nest">
-			<nav className="top-bar" role="navigation">
-				<div className="top-bar-title show-for-small-only">
-					<button className="fa fa-bars" type="button" onClick={this.toggleSubmenu.bind(this)} >
-					</button>
-				</div>
-				<div className={submenuClass}>
-					<MainMenuComponent role ={this.props.currentUser.role}/>
-				</div>
-				<section className="top-bar-section ">
-					<ul className="right">
-						<li className={accountMenuClass} onClick={this.toggleAccountMenu.bind(this)}>
-							<a className="bg-white" href="#">
-								<img alt="" className="admin-pic img-circle" src="/static/admin/images/no_avatar.jpg"/>
-								<span className="admin-pic-text text-gray">{userName}</span>
-							</a>
-							<ul
-								className="dropdown dropdown-nest profile-dropdown"
-								onMouseLeave={this.closeAccountMenu.bind(this)}
+class TopbarComponent extends React.Component <any, any>{
+	render(){
+		const userName = this.props.currentUser.name;
+		let submenuClass = 'top-bar-left show-for-small-only';
+		submenuClass += (this.props.isSubmenuOpen)?' open':'';
+		let accountMenuClass:string = 'has-dropdown bg-white';
+		accountMenuClass += (this.props.isAccountMenuOpen)?' open':'';
+		return(
+			<div className="top-bar-nest">
+				<nav className="top-bar" role="navigation">
+					<div className="top-bar-title show-for-small-only">
+						<button className="fa fa-bars" type="button" >
+						</button>
+					</div>
+					<div className={submenuClass}>
+						<MainMenuComponent role ={this.props.currentUser.role}/>
+					</div>
+					<section className="top-bar-section ">
+						<ul className="right">
+							<li className={accountMenuClass}>
+								<a className="bg-white" href="#">
+									<img alt="" className="admin-pic img-circle" src="/static/admin/images/no_avatar.jpg"/>
+									<span className="admin-pic-text text-gray">{userName}</span>
+								</a>
+								<ul
+									className="dropdown dropdown-nest profile-dropdown"
 								>
-								<li>
-									<i className="fa fa-sign-out"></i>
-									<a href="/logout">
-										<h4>
-											Logout
-										</h4>
-									</a>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</section>
-			</nav>
-		</div>
-	);
+									<li>
+										<i className="fa fa-sign-out"></i>
+										<a href="/logout">
+											<h4>
+												Logout
+											</h4>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</section>
+				</nav>
+			</div>
+		)
+	}
 };
 
 export default TopbarComponent;
