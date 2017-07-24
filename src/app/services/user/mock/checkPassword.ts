8c149c7d6acd84c144879d78dc25bb7fa1088e4f
Philipp Gröschler <phgroe@users.noreply.github.com>
2017-07-24 07:41:15 +0200

    Changed mock password with one that has only 10 rounds of bcrypt, to make testing faster

--- src/app/services/user/mock/checkPassword.ts
+++ src/app/services/user/mock/checkPassword.ts
@@ -1,10 +1,9 @@
 import * as bcrypt from 'bcrypt';
 
-// import { User } from 'base';
 import User from 'base/User';
 
 // Password for mock environment is "test"
-const adminPassword = '$2a$14$uH.TgIQi5Ed4RdRoFwWVaOBmnhAWDJYrnluZ6sMTj909ZDw7YTmpW';
+const adminPassword = '$2a$10$nrlEKXbpa9YQXacyUi6lN.Uj1y993hV9WDc5ZgFLNjz5c7mDevQt6';
 
 export default function (user : User, password : string) : Promise <boolean> {
 	if (user.name !== 'admin') {
