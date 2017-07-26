dd7eb6426c71a500a0fec306951c42e85033592d
Philipp <phgroe@gmx.net>
2017-07-27 01:21:37 +0200

    Generic type argument

--- src/app/services/user/UserService.ts
+++ src/app/services/user/UserService.ts
@@ -2,7 +2,7 @@ import { User } from 'base';
 
 import BasicPersistingService from '../BasicPersistingService';
 
-interface UserService extends BasicPersistingService {
+interface UserService extends BasicPersistingService <User> {
 	checkPassword(user : User, password : string) : Promise <boolean>;
 	get(username : string) : Promise <User>;
 }
