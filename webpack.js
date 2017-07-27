cb219201e57e8e2a050c4cbbeb1247764cb6f340
Philipp <phgroe@gmx.net>
2017-07-27 16:11:20 +0200

    Output webpack-bundle-analyzer reports to /stats

--- webpack.js
+++ webpack.js
@@ -140,12 +140,12 @@ module.exports = (applicationPart, scssParts) => {
 				// Webpack Stats JSON file by setting `generateStatsFile` to `true`.
 				analyzerMode: 'static',
 				// Host that will be used in `server` mode to start HTTP server.
-				analyzerHost: '127.0.0.1',
+				// analyzerHost: '127.0.0.1',
 				// Port that will be used in `server` mode to start HTTP server.
-				analyzerPort: 8888,
+				// analyzerPort: 8888,
 				// Path to bundle report file that will be generated in `static` mode.
 				// Relative to bundles output directory.
-				reportFilename: `webpack-report-${applicationPart}.html`,
+				reportFilename: path.join(__dirname, 'stats', `webpack-bundle-analyzer-${applicationPart}.html`),
 				// Module sizes to show in report by default.
 				// Should be one of `stat`, `parsed` or `gzip`.
 				// See "Definitions" section for more information.
@@ -154,11 +154,11 @@ module.exports = (applicationPart, scssParts) => {
 				openAnalyzer: false,
 				// If `true`, Webpack Stats JSON file will be generated in bundles
 				// output directory
-				generateStatsFile: false,
+				generateStatsFile: true,
 				// Name of Webpack Stats JSON file that will be generated if
 				// `generateStatsFile` is `true`.
 				// Relative to bundles output directory.
-				statsFilename: `webpack-stats-${applicationPart}.json`,
+				statsFilename: path.join(__dirname, 'stats', `webpack-bundle-analyzer-${applicationPart}.json`),
 				// Options for `stats.toJson()` method.
 				// For example you can exclude sources of your modules from stats file
 				// with `source: false` option.
