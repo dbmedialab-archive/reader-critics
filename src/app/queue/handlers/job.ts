8001a9c54c15a33d7931049a14668908c4b4a516
Philipp <philipp@sol.no>
2017-08-04 13:47:23 +0200

    Fixed missing logger

--- src/app/queue/handlers/job.ts
+++ src/app/queue/handlers/job.ts
@@ -3,6 +3,10 @@ import {
 	Job,
 } from 'kue';
 
+import * as app from 'app/util/applib';
+
+const log = app.createLog('api');
+
 export function onNewFeedback(job : Job, done : DoneCallback) {
 	log('New feedback in queue!', job.data);
 	done();
