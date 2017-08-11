4e49460756b1ba43578d673f0eda5ff7145d9640
Philipp <philipp@sol.no>
2017-08-11 11:07:45 +0200

    Populate article object on returned feedback. A problem persists with the referenced objects "website" and "articleAuthors", the stored IDs are not valid and point to nonexisting objects. Fix coming in new different branch.

--- src/app/services/feedback/FeedbackDAO.ts
+++ src/app/services/feedback/FeedbackDAO.ts
@@ -47,9 +47,13 @@ export function getByArticle(
 ) : Promise <Feedback[]> {
 	emptyCheck(article);
 
-	return wrapFind(FeedbackModel.find({
-		article: article.ID,
-	}).sort(sort).skip(skip).limit(limit));
+	return wrapFind(
+		FeedbackModel.find({
+			article: article.ID,
+		})
+		.sort(sort).skip(skip).limit(limit)
+		.populate('article')
+	);
 }
 
 // save
