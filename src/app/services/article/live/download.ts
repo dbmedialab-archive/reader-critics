a0877cda9dc3d8ca197011ac59e0d1382bcc7f93
Philipp <philipp@sol.no>
2017-09-29 10:47:04 +0200

    Fixed undefined reference to localization service

--- src/app/services/article/live/download.ts
+++ src/app/services/article/live/download.ts
@@ -27,11 +27,10 @@ import * as app from 'app/util/applib';
 
 import emptyCheck from 'app/util/emptyCheck';
 
-import { localizationService } from 'app/services';
+import { translate as __ } from 'app/services/localization';
 import { NotFoundError } from 'app/util/errors';
 
 const log = app.createLog();
-const __ = localizationService.translate;
 
 export default function(url : ArticleURL) : Promise <string> {
 	emptyCheck(url);
@@ -42,7 +41,7 @@ export default function(url : ArticleURL) : Promise <string> {
 	.catch(error => {
 		if (error.response) {
 			if (error.response.status === 404) {
-				return Promise.reject(new NotFoundError(__('no-article')));
+				return Promise.reject(new NotFoundError(__('err.no-article')));
 			}
 			return Promise.reject(new Error(`Article website returned a ${error.response.status} code`));
 		}
