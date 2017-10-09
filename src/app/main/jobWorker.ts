98a4853d88e194085c7b6b9f75190a56770e24b6
Philipp <phgr@gmx.net>
2017-10-09 11:18:10 +0200

    Removed mock feedback event

--- src/app/main/jobWorker.ts
+++ src/app/main/jobWorker.ts
@@ -25,18 +25,6 @@ import { initDatabase } from 'app/db';
 import { initJobWorkerQueue } from 'app/queue';
 import { initLocalizationStrings } from 'app/services/localization';
 
-/* !!! >>>>> */
-import Article from 'base/Article';
-import Feedback from 'base/Feedback';
-
-import onNewFeedback from 'app/queue/handlers/onNewFeedback';
-
-import {
-	articleService,
-	feedbackService,
-} from 'app/services';
-/* <<<<< !!! */
-
 import startupErrorHandler from './startupErrorHandler';
 
 let log;
@@ -54,38 +42,5 @@ export default function() {
 		.then(initLocalizationStrings)
 		.then(initDatabase)
 		.then(initJobWorkerQueue)
-
-/* !!! >>>>> ####################################################### >>>>>*/
-		.then(() => {
-			log('### MOCK new feedback event #######################################################');
-
-			// Get an article from the test crowd
-			return articleService.get('http://www.mopo.no/2', '201707251349')
-			// Get first feedback for this article => test contents
-			.then((article : Article) => {
-				if (article === null) {
-					return Promise.reject('Test article not found');
-				}
-				return feedbackService.getByArticle(article, 0, 1, {
-					'date.created': 1,
-				});
-			})
-			// Check result and get feedback from array
-			.then((results : Feedback[]) => {
-				if (results.length !== 1) {
-					return Promise.reject('Test feedback not found');
-				}
-				return Promise.resolve(results[0]);
-			})
-			// Send fake event to job processor
-			.then((feedback : Feedback) => onNewFeedback({
-				data: feedback,
-			}, () => {
-				log('done() callback');
-				log('### MOCK feedback event finished ##################################################');
-			}));
-		})
-/* <<<<< ####################################################### <<<<< !!! */
-
 		.catch(startupErrorHandler);
 }
