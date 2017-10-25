8b0783818b274a6efde811a8cf51b607036d1def
Philipp <phgroe@gmx.net>
2017-10-25 11:26:51 +0200

    Removed TODO

--- src/app/services/user/UserService.ts
+++ src/app/services/user/UserService.ts
@@ -5,21 +5,21 @@ import BasicPersistingService from '../BasicPersistingService';
 
 interface UserService extends BasicPersistingService <User> {
 	checkPassword(user : User, password : string) : Promise <boolean>;
+	setPasswordHash(user : User, password: string) : Promise <User>;
+
 	doDelete(id: String);
+
 	get(username : String, email? : String|null) : Promise <User>;
 	getByEmail(email : String) : Promise <User>;
 	getByID(id : String) : Promise <User>;
+
 	save(user : User) : Promise <User>;
 
 	findOrInsert(user : Person) : Promise <User>;
 	update(id: String, user : Person) : Promise <User>;
 
-	/**
-	 * TODO: reference to the same method from FeedbackService
-	 */
 	validateAndSave(data : any) : Promise <User>;
 	validateAndUpdate(id: String, data : Object) : Promise <User>;
-	setPasswordHash(user : User, password: string) : Promise <User>;
 }
 
 export default UserService;
