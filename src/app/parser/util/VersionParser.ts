47c3af5c6dceeaece221b1ce079f399bbf7a8009
Philipp <phgroe@gmx.net>
2017-11-27 14:42:57 +0100

    Optional parameter for loose matching in version parser

--- src/app/parser/util/VersionParser.ts
+++ src/app/parser/util/VersionParser.ts
@@ -22,11 +22,25 @@ import * as Cheerio from 'cheerio';
  * Facebook's OpenGraph scheme
  * looks for <meta property="article:modified_time" content="...">
  */
-export function getOpenGraphModifiedTime(select : Cheerio) : string {
-	let meta = select('meta[property="article:modified_time"]').toArray();
+export function getOpenGraphModifiedTime(
+	select : Cheerio,
+	looseMatching : boolean = false
+) : string {
+	let meta;
 
-	if (meta.length === 0) {  // Fallback for sites that use the wrong attribute
-		meta = select('meta[name="article:modified_time"]').toArray();
+	if (looseMatching) {
+		meta = select('meta[property="article:modified_time"]').toArray();
+
+		if (meta.length === 0) {  // Fallback for sites that use the wrong attribute
+			meta = select('meta[name="article:modified_time"]').toArray();
+		}
+	}
+	else {
+		meta = select('head').find('meta[property="article:modified_time"]');
+
+		if (meta.length === 0) {  // Fallback for sites that use the wrong attribute
+			meta = select('head').find('meta[name="article:modified_time"]');
+		}
 	}
 
 	return meta.length < 1 ? undefined : select(meta[0]).attr('content');
