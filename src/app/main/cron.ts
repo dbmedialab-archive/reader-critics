5df2e7e25dd61ff9999884c1a216ebe209b0d6aa
Philipp <phgroe@gmx.net>
2018-01-29 18:43:06 +0100

    Use wrapper function

--- src/app/main/cron.ts
+++ src/app/main/cron.ts
@@ -17,6 +17,7 @@
 //
 
 import { CronJob } from 'cron';
+import { wrapCronFn } from './vote';
 
 import {
 	sendMessage,
@@ -32,10 +33,8 @@ const activeJobs : Array <CronJob> = [];
 export function initCron() : Promise <void> {
 	log('Initialising ...');
 
-	/*
 	jobCheckAwaitFeedback();
 	jobCollectArticlesForPolling();
-	*/
 
 	return Promise.resolve();
 }
@@ -43,7 +42,9 @@ export function initCron() : Promise <void> {
 function jobCheckAwaitFeedback() {
 	activeJobs.push(new CronJob({
 		cronTime: '0 * * * * *',  // Every minute
-		onTick: () => sendMessage(MessageType.CheckAwaitFeedback),
+		onTick: () => wrapCronFn(() => {
+			sendMessage(MessageType.CheckAwaitFeedback);
+		}),
 		start: true,
 	}));
 }
@@ -51,7 +52,9 @@ function jobCheckAwaitFeedback() {
 function jobCollectArticlesForPolling() {
 	activeJobs.push(new CronJob({
 		cronTime: '0 30 * * * *',  // Every hour at xx:30
-		onTick: () => sendMessage(MessageType.CollectArticlesForPolling),
+		onTick: () => wrapCronFn(() => {
+			sendMessage(MessageType.CollectArticlesForPolling);
+		}),
 		start: true,
 	}));
 }
